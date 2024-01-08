1a9d24a0df2d78d47c88fe01effeca740fd2a0e2
Elias Velazquez <evelazquez2604@gmail.com>
2024-01-08 14:34:45 -0300

    refactor: Add menu on main file. Move scraping functionality to scraper script.

---  main.py
+++  main.py
@@ -1,31 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+# Menu
 
-driver = webdriver.Chrome()
-driver.get("https://www.cotodigital3.com.ar/sitios/cdigi/")
+print("Bienvenido/a al scraper de precios.")
+print("Estos son los productos disponibles:")
+print("")
+print("1. Gaseosa Coca Cola 2.5lt")
+print("2. Gaseosa Coca Cola 2.25lt")
+print("3. Gaseosa Coca Cola Sabor Original 500ml")
 
-# Wait 10 seconds until the element (search bar) is found and clickable
-try:
-    searchInput = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//div[contains(@class, "atg_store_search_max_length")]/input[@id="atg_store_searchInput"]'))
-    )
-except Exception as e:
-    print(e)
+print("")
+option = input("Selecciona el producto del cual te gustaría recibir información: ")
+product = None
 
-    
-searchInput.clear()
-searchInput.send_keys("gaseosa coca cola 2.5")
-searchInput.click()
-time.sleep(1)
-searchInput.send_keys(Keys.ARROW_DOWN)
-time.sleep(1)
-searchInput.send_keys(Keys.ENTER)
-time.sleep(3)
-
-
-
-driver.close()
+match option:
+    case "1":
+        product = "Gaseosa Coca Cola 2.5lt"
+    case "2":
+        product = "Gaseosa Coca Cola 2.25lt"
+    case "3":
+        product = "Gaseosa Coca Cola Sabor Original 500ml"
---
+++ scraper.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.cotodigital3.com.ar/sitios/cdigi/")
+
+# Wait 10 seconds until the element (search bar) is found and clickable
+try:
+    searchInput = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//div[contains(@class, "atg_store_search_max_length")]/input[@id="atg_store_searchInput"]'))
+    )
+except Exception as e:
+    print(e)
+
+    
+searchInput.clear()
+searchInput.send_keys("gaseosa coca cola 2.5")
+searchInput.click()
+time.sleep(1)
+searchInput.send_keys(Keys.ARROW_DOWN)
+time.sleep(1)
+searchInput.send_keys(Keys.ENTER)
+time.sleep(3)
+
+current_url = driver.current_url
+
+
+
+
+driver.close()

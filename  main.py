4410245ffd6ab6089ce7ebaeb48925b4c0071579
Elias Velazquez <evelazquez2604@gmail.com>
2024-01-12 16:11:51 -0300

    refactor: Change export to csv function in scraper script
    This commits changes the type of argument 'export_product_info_to_csv' receives from dict to list. It also adds a while statement to the main script to allow users to pick one than more product for data retrieval.

---  main.py
+++  main.py
@@ -4,43 +4,69 @@ import time
 
 # Menu
 print("Bienvenido/a al scraper de precios.")
-print("Estos son los productos disponibles:")
+
+choice = True
+
+while(choice):
+    print("Estos son los productos disponibles:")
+    print("")
+    print("1. Gaseosa Coca Cola 2.5lt")
+    print("2. Gaseosa Coca Cola 2.25lt")
+    print("3. Gaseosa Coca Cola Sabor Original 500ml")
+
+    print("")
+    option = input("Selecciona el producto del cual te gustaría recibir información. Introduce el numero correspondiente: ")
+
+    print("Consiguiendo información...")
+    product = None
+
+    match option:
+        case "1":
+            product = "Gaseosa Coca Cola 2.5 L"
+        case "2":
+            product = "Gaseosa Coca Cola 2.25 L"
+        case "3":
+            product = "Gaseosa Coca Cola Sabor Original 500 Ml"
+
+
+    product_list = []
+
+    # Get product's information from website
+    product_info = get_product_info(product)
+
+    # Check if the product has been previously consulted and is part of the list of products
+    if product_info not in product_list:
+        product_list.append(product_info)
+        print("Informacion de producto obtenida correctamente.")
+    else:
+        print("Este producto fue consultado previamente.")
+
+    print("")
+    print("¿Te gustaría consultar otro producto?")
+    answer = input("Sí (s)/No (n): ").lower()
+    match answer:
+        case "s":
+            pass
+        case "n":
+            choice = False
+
+
+print("Esta es la informacion obtenida del/los producto/s seleccionado/s: ")
 print("")
-print("1. Gaseosa Coca Cola 2.5lt")
-print("2. Gaseosa Coca Cola 2.25lt")
-print("3. Gaseosa Coca Cola Sabor Original 500ml")
 
-print("")
-option = input("Selecciona el producto del cual te gustaría recibir información. Introduce el numero correspondiente: ")
-
-print("Consiguiendo información...")
-product = None
-
-match option:
-    case "1":
-        product = "Gaseosa Coca Cola 2.5 L"
-    case "2":
-        product = "Gaseosa Coca Cola 2.25 L"
-    case "3":
-        product = "Gaseosa Coca Cola Sabor Original 500 Ml"
-
-
-product_info = get_product_info(product)
-
-print("Esta es la informacion obtenida del producto seleccionado: ")
-print("")
-print(
-    f'Nombre: {product_info["name"]}\nDescripcion: {product_info["description"]}\nPrecio: {product_info["price"]}\nCategoria: {product_info["category"]}\nImagen: {product_info["picture"]}\nEnlace: {product_info["url"]}'
-)
+for product in product_list:
+    print(
+        f'Nombre: {product["name"]}\nDescripcion: {product["description"]}\nPrecio: {product["price"]}\nCategoria: {product["category"]}\nImagen: {product["picture"]}\nEnlace: {product["url"]}'
+    )
 
 print("")
 print("¿Te gustaría exportar la informacion en un archivo CSV?")
-answer = input("Sí (s)/No (n): ").lower()
+answer_2 = input("Sí (s)/No (n): ").lower()
 
-match answer:
+match answer_2:
     case "s":
         print("Exportando...")
-        if export_product_info_to_csv(product_info):
+        if export_product_info_to_csv(product_list):
             print("¡La información ha sido exportada correctamente!")
         else:
             print("Ha ocurrido un problema. Por favor intentalo de nuevo.")
--- data.py
+++ data.py
@@ -5,24 +5,26 @@ def main_greeting():
     return "Executing script"
 
 
-def export_product_info_to_csv(product_info: dict):
+def export_product_info_to_csv(product_list: list) -> bool:
     """
-    Takes a dictionary with product information as an argument and saves values into a CSV file
+    Takes a list with dictionaries containing product information as an argument and saves values into a CSV file
+    Returns true is the csv export is successful
     """
     try:
-        with open('productos.csv', 'w') as csvfile:
+        with open('productos.csv', 'a+') as csvfile:
             fieldnames = ["Producto", "Descripcion", "Precio", "Categoria", "Imagen", "Enlace"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             # Write header (first row)
             writer.writeheader()
-            writer.writerow({
-                "Producto" : product_info["name"],
-                "Descripcion" : product_info["description"],
-                "Precio" : product_info["price"],
-                "Categoria" : product_info["category"],
-                "Imagen" : product_info["picture"],
-                "Enlace" : product_info["url"]
-            })
+            for product_info in product_list:
+                writer.writerow({
+                    "Producto" : product_info["name"],
+                    "Descripcion" : product_info["description"],
+                    "Precio" : product_info["price"],
+                    "Categoria" : product_info["category"],
+                    "Imagen" : product_info["picture"],
+                    "Enlace" : product_info["url"]
+                })
             return True
     except Exception as e:
         print("Error!", e)
